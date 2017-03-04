5e15c13ce9350dca41d2b0f6e2360cf0a93c2564
pwfff <aaron.lisman@gmail.com>
2017-03-04 11:59:04 -0600

    initial commit

---
+++ goonpug/GoonPugApplicationContext.cs
@@ -0,0 +1,139 @@
+﻿using SSQLib;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace goonpug
+{
+    class GoonPugApplicationContext : ApplicationContext
+    {
+        private NotifyIcon TrayIcon;
+        private ContextMenuStrip TrayIconContextMenu;
+        private ToolStripMenuItem CloseMenuItem;
+
+        private readonly SynchronizationContext synchronizationContext;
+        private readonly ServerUpdater serverUpdater;
+
+        public GoonPugApplicationContext()
+        {
+            serverUpdater = new ServerUpdater();
+            serverUpdater.ServerUpdate += OnServerUpdate;
+
+            Application.ApplicationExit += new EventHandler(OnApplicationExit);
+
+            synchronizationContext = SynchronizationContext.Current;
+
+            InitializeComponent();
+
+            TrayIcon.Visible = true;
+        }
+
+        private void InitializeComponent()
+        {
+            TrayIcon = new NotifyIcon();
+
+            TrayIcon.BalloonTipIcon = ToolTipIcon.Info;
+            TrayIcon.Text = "sub.io Server Status";
+            
+            TrayIcon.Icon = Properties.Resources.TrayIcon;
+            
+            //Optional - Add a context menu to the TrayIcon:
+            TrayIconContextMenu = new ContextMenuStrip();
+            CloseMenuItem = new ToolStripMenuItem();
+            TrayIconContextMenu.SuspendLayout();
+
+            // 
+            // CloseMenuItem
+            // 
+            CloseMenuItem.Name = "Close";
+            CloseMenuItem.Size = new Size(152, 22);
+            CloseMenuItem.Text = "Exit";
+            CloseMenuItem.Click += new EventHandler(CloseMenuItem_Click);
+            // 
+            // TrayIconContextMenu
+            // 
+            TrayIconContextMenu.Items.AddRange(new ToolStripItem[] { CloseMenuItem });
+            TrayIconContextMenu.Name = "Server Statuses";
+            TrayIconContextMenu.Size = new Size(153, 70);
+
+            TrayIconContextMenu.ResumeLayout(false);
+            TrayIcon.ContextMenuStrip = TrayIconContextMenu;
+
+            Task.Run(() => serverUpdater.PollServers());
+        }
+
+        private void OnServerUpdate(object sender, ServerUpdateEventArgs e)
+        {
+            if (TrayIconContextMenu.InvokeRequired)
+            {
+                TrayIconContextMenu.Invoke(new Action<object, ServerUpdateEventArgs>(OnServerUpdate), sender, e);
+                return;
+            }
+
+            if (e.SendAlert)
+            {
+                var alertText = "goonman players needed!\n" + string.Join("\n", e.ServerInfos.Select(s => s.Info()));
+                TrayIcon.ShowBalloonTip(10000, "sub.io", alertText, ToolTipIcon.Warning);
+            }
+
+            TrayIconContextMenu.Items.Clear();
+
+            var menuItems = e.ServerInfos.Select(server => ServerToolStripMenuItem.GetToolStripMenuItem(server, new EventHandler(OnServerClick)));
+            TrayIconContextMenu.Items.AddRange(menuItems.ToArray());
+
+            TrayIconContextMenu.Items.Add(CloseMenuItem);
+        }
+
+        private void OnApplicationExit(object sender, EventArgs e)
+        {
+            //Cleanup so that the icon will be removed when the application is closed
+            TrayIcon.Visible = false;
+        }
+        
+        private void OnServerClick(object sender, EventArgs e)
+        {
+            var menuItem = (ServerToolStripMenuItem)sender;
+            System.Diagnostics.Process.Start(
+                string.Format("steam://run/730//+connect {0}:{1}", ServerUpdater.Hostname, menuItem.server.Port)
+            );
+        }
+
+        private void CloseMenuItem_Click(object sender, EventArgs e)
+        {
+                Application.Exit();
+        }
+    }
+
+    public class ServerToolStripMenuItem : ToolStripMenuItem
+    {
+        public readonly ServerInfo server;
+
+        public ServerToolStripMenuItem(string text, Image image, EventHandler onClick, string name, ServerInfo server)
+            : base(text, image, onClick, name)
+        {
+            this.server = server;
+        }
+
+        public static ToolStripMenuItem GetToolStripMenuItem(ServerInfo server, EventHandler onClick)
+        {
+            return new ServerToolStripMenuItem(
+                server.Info(),
+                null,
+                onClick,
+                server.Name,
+                server
+            );
+        }
+    }
+
+    public static class ServerInfoExtensions
+    {
+        public static string Info(this ServerInfo server)
+        {
+            return string.Format("{0}: {1}/{2}", server.Name, server.PlayerCount, server.MaxPlayers);
+        }
+    }
+}
---
+++ goonpug/ServerUpdater.cs
@@ -0,0 +1,94 @@
+﻿using SSQLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+
+namespace goonpug
+{
+    public delegate void ServerUpdateEventHandler(object sender, ServerUpdateEventArgs e);
+
+    public class ServerUpdateEventArgs : EventArgs
+    {
+        public readonly IEnumerable<ServerInfo> ServerInfos;
+        public readonly bool SendAlert;
+
+        public ServerUpdateEventArgs(IEnumerable<ServerInfo> serverInfos, bool sendAlert)
+        {
+            ServerInfos = serverInfos;
+            SendAlert = sendAlert;
+        }
+    }
+
+    class ServerUpdater
+    {
+        public const string Hostname = "csgo.sub.io";
+        public event ServerUpdateEventHandler ServerUpdate;
+
+        private readonly IEnumerable<IPEndPoint> Endpoints;
+
+#if DEBUG
+        private const int MinPlayers = 0;
+        private const int AlertMinutes = 1;
+#else
+        private const int MinPlayers = 5;
+        private const int AlertMinutes = 10;
+#endif
+
+        private DateTime lastAlert;
+
+        public ServerUpdater()
+        {
+            var address = Dns.GetHostEntry(Hostname).AddressList.First();
+
+            Endpoints = new List<IPEndPoint> { new IPEndPoint(address, 27015), new IPEndPoint(address, 27017) };
+
+        }
+
+        public void PollServers()
+        {
+            SSQL query = new SSQL();
+
+            while (true)
+            {
+                try
+                {
+                    var serverInfos = Endpoints.Select(query.Server);
+
+                    bool alert = false;
+                    if (serverInfos.Any(ServerNeedsPlayers) && NeedsAlert())
+                    {
+                        lastAlert = DateTime.UtcNow;
+                        alert = true;
+                    };
+
+                    ServerUpdate(this, new ServerUpdateEventArgs(serverInfos, alert));
+                    Thread.Sleep(30000);
+                }
+                catch
+                {
+                    ServerUpdate(this, new ServerUpdateEventArgs(
+                        new ServerInfo[] { new ServerInfo() {
+                            Name = "Error fetching server info.",
+                            PlayerCount = "0",
+                            MaxPlayers = "0"
+                        } },
+                        false
+                    ));
+                }
+            }
+        }
+        
+        private static bool ServerNeedsPlayers(ServerInfo server)
+        {
+            var players = int.Parse(server.PlayerCount);
+            return players > MinPlayers && players < 11;
+        }
+
+        private bool NeedsAlert()
+        {
+            return DateTime.UtcNow - lastAlert > TimeSpan.FromMinutes(AlertMinutes);
+        }
+    }
+}
